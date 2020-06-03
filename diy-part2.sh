170f121bad8f985ad2b86f7df31e1a838318a425
jiangcm <jiangcm@users.noreply.github.com>
2020-06-03 08:02:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 echo  0xDEADBEEF /feeds/helloworld/luci-app-ssr-plus/root/etc/config/google_fu_mode
 git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/me
+git clone https://github.com/awesome-openwrt/luci-app-control-timewol.git package/me
+git clone https://github.com/awesome-openwrt/luci-app-control-mia.git package/me
+git clone https://github.com/awesome-openwrt/luci-app-control-weburl.git package/me
+git clone https://github.com/awesome-openwrt/luci-app-control-webrestriction.git package/me

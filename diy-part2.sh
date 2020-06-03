d8cc45327111d1e24b2611cd6ad93e557e24dbad
jiangcm <jiangcm@users.noreply.github.com>
2020-06-03 08:27:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 echo  0xDEADBEEF /feeds/helloworld/luci-app-ssr-plus/root/etc/config/google_fu_mode
-git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/me
-git clone https://github.com/awesome-openwrt/luci-app-control-timewol.git package/me
-git clone https://github.com/awesome-openwrt/luci-app-control-mia.git package/me
-git clone https://github.com/awesome-openwrt/luci-app-control-weburl.git package/me
-git clone https://github.com/awesome-openwrt/luci-app-control-webrestriction.git package/me
+git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/me/luci-theme-atmaterial
+git clone https://github.com/awesome-openwrt/luci-app-control-timewol.git package/me/luci-app-control-timewol
+git clone https://github.com/awesome-openwrt/luci-app-control-mia.git package/me/luci-app-control-mia
+git clone https://github.com/awesome-openwrt/luci-app-control-weburl.git package/me/luci-app-control-weburl
+git clone https://github.com/awesome-openwrt/luci-app-control-webrestriction.git package/me/luci-app-control-webrestriction
